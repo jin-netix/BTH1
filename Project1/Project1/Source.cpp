b1cd444dde08c49463a5cf63aa1b1490f36c1e23
thanhdat0 <2251052016dat@ou.edu.vn>
2025-03-01 16:46:30 +0700

    ++

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -16,38 +16,8 @@ bool SoNguyenTo(int n) {
 	}
 }
 
-bool nam_nhuan(int year)
-{
-	if (year % 100 == 0)
-	{
-		if (year % 400 == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	else if (year % 4 == 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
+
 
 int main() {
-	int n;
-	cin >> n;
-	if (SoNguyenTo(n))
-		cout << n << " la so nguyen to\n";
-	else
-		cout << n << " khong phai la so nguyen to\n";
-
-	int a;
-	cin >> a;
-	if (nam_nhuan(a))
-		cout << a << " la nam nhuan\n";
-	else
-		cout << a << " khong phai la nam nhuan\n";
 
 }
